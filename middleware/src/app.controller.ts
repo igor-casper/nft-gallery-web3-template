6a521271613ca70dbb6cbc72a1ba709b1ecfe1e9
igor-casper <igor@casper.network>
2023-12-08 17:08:39 +0100

    move eventstream to naive http, fix controller
    - honor env vars in app.controller, they were ignored previously (hardcoded)
    - the js sdk seems to have broken event streams, temporarily replaced these with a naive implementation over http

--- middleware/src/app.controller.ts
+++ middleware/src/app.controller.ts
@@ -8,67 +8,69 @@ import { TicketSubmitDto } from './types';
 import { ERROR_CODES } from './constants';
 import { toJS } from './utils';
 
+const { TOTAL_NFTS_NUM } = process.env;
+
 @Controller('app')
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly casperService: CasperService,
-  ) {
-    const bootstrap = async () => {
-      const ids = Array.from(Array(100).keys()).map((i) => `${i + 1}`);
-      const allNFTs = await this.casperService.listNfts(ids);
-      this.appService.setOrUpdateNFTs(allNFTs);
-    };
+	constructor(
+		private readonly appService: AppService,
+		private readonly casperService: CasperService,
+	) {
+		const bootstrap = async () => {
+			const ids = Array.from(Array(TOTAL_NFTS_NUM).keys()).map((i) => `${i + 1}`);
+			const allNFTs = await this.casperService.listNfts(ids);
+			this.appService.setOrUpdateNFTs(allNFTs);
+		};
 
-    bootstrap();
-  }
+		bootstrap();
+	}
 
-  @Get()
-  async getAllNFTs(): Promise<NFT[]> {
-    const serverPKHash = this.casperService.publicKey().toAccountHashStr();
-    return this.appService.getAllNFTs(serverPKHash);
-  }
+	@Get()
+	async getAllNFTs(): Promise<NFT[]> {
+		const serverPKHash = this.casperService.publicKey().toAccountHashStr();
+		return this.appService.getAllNFTs(serverPKHash);
+	}
 
-  @Get('status/:accountHash')
-  async findOwned(@Param() params) {
-    const { accountHash } = params;
-    return toJS(await this.appService.checkStatus(accountHash));
-  }
+	@Get('status/:accountHash')
+	async findOwned(@Param() params) {
+		const { accountHash } = params;
+		return toJS(await this.appService.checkStatus(accountHash));
+	}
 
-  @Patch()
-  async transfer(@Body() ticketSubmitDto: TicketSubmitDto) {
-    const { accountHash } = ticketSubmitDto;
-    const serverPublicKey = this.casperService.publicKey();
-    const maybeNFT = await this.appService.findFreeNFT(
-      ticketSubmitDto,
-      serverPublicKey.toAccountHashStr(),
-    );
+	@Patch()
+	async transfer(@Body() ticketSubmitDto: TicketSubmitDto) {
+		const { accountHash } = ticketSubmitDto;
+		const serverPublicKey = this.casperService.publicKey();
+		const maybeNFT = await this.appService.findFreeNFT(
+			ticketSubmitDto,
+			serverPublicKey.toAccountHashStr(),
+		);
 
-    if (maybeNFT.ok) {
-      const [nft, ticket] = maybeNFT.unwrap();
-      nft.blocked = true;
-      nft.save();
-      const maybeHash = await this.casperService.transferNFT(nft, accountHash);
+		if (maybeNFT.ok) {
+			const [nft, ticket] = maybeNFT.unwrap();
+			nft.blocked = true;
+			nft.save();
+			const maybeHash = await this.casperService.transferNFT(nft, accountHash);
 
-      if (maybeHash.ok) {
-        const hash = maybeHash.unwrap();
-        // TODO: add deploy
-        const deploy = await this.appService.addDeploy(
-          hash,
-          accountHash,
-          nft.id,
-        );
-        ticket.deploys.push(deploy);
-        ticket.save();
-      } else {
-        nft.blocked = false;
-        nft.save();
-        ticket.isUsed = false;
-        ticket.save();
-      }
-      return toJS(maybeHash);
-    } else {
-      return toJS(maybeNFT);
-    }
-  }
+			if (maybeHash.ok) {
+				const hash = maybeHash.unwrap();
+				// TODO: add deploy
+				const deploy = await this.appService.addDeploy(
+					hash,
+					accountHash,
+					nft.id,
+				);
+				ticket.deploys.push(deploy);
+				ticket.save();
+			} else {
+				nft.blocked = false;
+				nft.save();
+				ticket.isUsed = false;
+				ticket.save();
+			}
+			return toJS(maybeHash);
+		} else {
+			return toJS(maybeNFT);
+		}
+	}
 }
--- middleware/src/casper.service.ts
+++ middleware/src/casper.service.ts
@@ -2,22 +2,22 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { Ok, Err, Result } from 'ts-results';
 import { InjectModel } from '@nestjs/mongoose';
+import { ClientRequest } from 'http';
+import * as http from 'http';
 
 import {
-  CasperClient,
-  EventStream,
-  EventName,
-  CLValueBuilder,
-  CLPublicKey,
-  CLAccountHash,
-  Keys,
+	CasperClient,
+	EventName,
+	CLPublicKey,
+	CLAccountHash,
+	Keys,
 } from 'casper-js-sdk';
 
 import {
-  CEP47Client,
-  CEP47Events,
-  CEP47EventParser,
-} from 'casper-cep47-js-client';
+	CEP47Events,
+	CEP47EventParserFactory,
+	CEP78Client
+} from 'casper-cep78-js-client'
 
 import { Deploy, DeployDocument, DEPLOY_STATUS } from './schemas/deploy.schema';
 import { Ticket, TicketDocument } from './schemas/ticket.schema';
@@ -26,164 +26,185 @@ import { getKeysFromHexPrivKey } from './utils';
 import { KEY_VARIANTS, ERROR_CODES } from './constants';
 
 const {
-  CHAIN_RPC_URL,
-  CHAIN_NET_NAME,
-  CHAIN_EE_URL,
-  CHAIN_CONTRACT_ADDR,
-  CHAIN_CONTRACT_PACKAGE_ADDR,
-  CHAIN_PRIV_KEY_VARIANT,
-  CHAIN_PRIV_KEY_BASE64,
+	CHAIN_RPC_URL,
+	CHAIN_NET_NAME,
+	CHAIN_EE_URL,
+	CHAIN_CONTRACT_ADDR,
+	CHAIN_CONTRACT_PACKAGE_ADDR,
+	CHAIN_PRIV_KEY_VARIANT,
+	CHAIN_PRIV_KEY_BASE64,
 } = process.env;
 
 @Injectable()
 export class CasperService {
-  private casperClient: CasperClient;
-  private contractClient: CEP47Client;
-  private eventStream: EventStream;
-  private keyPair: Keys.AsymmetricKey;
-  private pendingHashes = [];
+	private casperClient: CasperClient;
+	private contractClient: CEP78Client;
+	private eventStream: ClientRequest;
+	private keyPair: Keys.AsymmetricKey;
+	private pendingHashes = [];
 
-  constructor(
-    @InjectModel(Deploy.name) private deployModel: Model<DeployDocument>,
-    @InjectModel(NFT.name) private nftModel: Model<NFTDocument>,
-    @InjectModel(Ticket.name) private ticketModel: Model<TicketDocument>,
-  ) {
-    this.casperClient = new CasperClient(CHAIN_RPC_URL);
-    this.contractClient = new CEP47Client(CHAIN_RPC_URL, CHAIN_NET_NAME);
-    this.eventStream = new EventStream(CHAIN_EE_URL);
+	constructor(
+		@InjectModel(Deploy.name) private deployModel: Model<DeployDocument>,
+		@InjectModel(NFT.name) private nftModel: Model<NFTDocument>,
+		@InjectModel(Ticket.name) private ticketModel: Model<TicketDocument>,
+	) {
+		this.casperClient = new CasperClient(CHAIN_RPC_URL);
+		this.contractClient = new CEP78Client(CHAIN_RPC_URL, CHAIN_NET_NAME);
+		this.eventStream = http.get(CHAIN_EE_URL, result => {
+			result.on('data', chunk => {
+				try {
+					chunk = chunk.toString();
+					chunk = chunk.replace("data:","").split('\n')[0];
+					let body = JSON.parse(chunk);
+					if (body[EventName.DeployProcessed])
+						processDeploy(body);
+				} catch(e) {
+					console.log(`Caught invalid data: ${e}`)
+				}
+			})
+		});
 
-    this.contractClient.setContractHash(
-      CHAIN_CONTRACT_ADDR,
-      CHAIN_CONTRACT_PACKAGE_ADDR,
-    );
+		const CEP47EventParser = CEP47EventParserFactory({
+			contractPackageHash: CHAIN_CONTRACT_PACKAGE_ADDR,
+			eventNames: [
+				CEP47Events.Mint,
+				CEP47Events.Transfer,
+				CEP47Events.Burn,
+			],
+		});
 
-    this.keyPair = getKeysFromHexPrivKey(
-      CHAIN_PRIV_KEY_BASE64,
-      KEY_VARIANTS[CHAIN_PRIV_KEY_VARIANT],
-    );
+		this.contractClient.setContractHash(
+			CHAIN_CONTRACT_ADDR,
+			CHAIN_CONTRACT_PACKAGE_ADDR,
+		);
 
-    const bootstrapPendingDeploys = async () => {
-      const list = await this.deployModel
-        .find({ status: DEPLOY_STATUS.Pending })
-        .exec();
-      list.forEach(
-        (d) =>
-          d.status === DEPLOY_STATUS.Pending && this.pendingHashes.push(d.hash),
-      );
-    };
+		this.keyPair = getKeysFromHexPrivKey(
+			CHAIN_PRIV_KEY_BASE64,
+			KEY_VARIANTS[CHAIN_PRIV_KEY_VARIANT],
+		);
 
-    bootstrapPendingDeploys();
+		const bootstrapPendingDeploys = async () => {
+			const list = await this.deployModel
+				.find({ status: DEPLOY_STATUS.Pending })
+				.exec();
+			list.forEach(
+				(d) =>
+					d.status === DEPLOY_STATUS.Pending && this.pendingHashes.push(d.hash),
+			);
+		};
 
-    this.eventStream.subscribe(EventName.DeployProcessed, async (event) => {
-      const { deploy_hash, execution_result } = event.body.DeployProcessed;
+		bootstrapPendingDeploys();
 
-      if (this.pendingHashes.includes(deploy_hash)) {
-        if (execution_result.Success) {
-          this.pendingHashes = this.pendingHashes.filter(
-            (a) => a !== deploy_hash,
-          );
-          await this.deployModel.update(
-            { hash: deploy_hash },
-            { $set: { status: DEPLOY_STATUS.Succeeded } },
-          );
-        } else {
-          const deploy = await this.deployModel.findOne({ 
-            hash: deploy_hash
-          });
-          deploy.status = DEPLOY_STATUS.Failed;
-          await deploy.save();
-          const ticket = await this.ticketModel.findOne({ deploys: deploy });
-          ticket.isUsed = false;
-          await ticket.save();
-        }
-      }
+		const processDeploy = async event => {
+			const { deploy_hash, execution_result } = event.DeployProcessed;
 
-      const parsedEvents = CEP47EventParser(
-        {
-          contractPackageHash: CHAIN_CONTRACT_PACKAGE_ADDR,
-          eventNames: [CEP47Events.TransferToken],
-        },
-        event,
-      );
+			if(this.pendingHashes.includes(deploy_hash)) {
+				if(execution_result.Success) {
+					this.pendingHashes = this.pendingHashes.filter(
+						a => a !== deploy_hash,
+					);
+					await this.deployModel.updateMany(
+						{ hash: deploy_hash },
+						{ $set: { status: DEPLOY_STATUS.Succeeded } },
+					);
+				}
+			} else {
+				const deploy = await this.deployModel.findOne({
+					hash: deploy_hash
+				});
+				deploy.status = DEPLOY_STATUS.Failed;
+				await deploy.save();
+				const ticket = await this.ticketModel.findOne({ deploys: deploy });
+				ticket.isUsed = false;
+				await ticket.save();
+			}
 
-      if (parsedEvents && parsedEvents.success) {
-        parsedEvents.data.forEach(async (parsedEvent) => {
-          if (parsedEvent.name === CEP47Events.TransferToken) {
-            const { clValue } = parsedEvent;
+			const parsedEvents = CEP47EventParser(event);
 
-            const deployRecepientHashRaw = clValue
-              .get(CLValueBuilder.string('recipient'))
-              .value()
-              .slice(13, -1);
+			if (parsedEvents && parsedEvents.success) {
+				parsedEvents.data.forEach(async (parsedEvent) => {
+					if (parsedEvent.name === CEP47Events.Transfer) {
+						const clValue = parsedEvent.clValue;
 
-            const deployTokenId = clValue
-              .get(CLValueBuilder.string('token_id'))
-              .value();
+						console.log("===== Transfer =====")
+						console.log(clValue);
+						console.log(clValue.data);
 
-            await this.nftModel.updateOne(
-              { id: deployTokenId },
-              {
-                owner: `account-hash-${deployRecepientHashRaw}`,
-                blocked: false,
-              },
-            );
+						/*const deployRecepientHashRaw = clValue
+							.get(CLValueBuilder.string('recipient'))
+							.value()
+							.slice(13, -1);
 
-            console.log('Transfered token: ', deployTokenId);
-            console.log('Transfered token recipient: ', deployRecepientHashRaw);
-          }
-        });
-      }
-    });
+						const deployTokenId = clValue
+							.get(CLValueBuilder.string('token_id'))
+							.value();
 
-    this.eventStream.start();
-  }
+						await this.nftModel.updateOne(
+							{ id: deployTokenId },
+							{
+								owner: `account-hash-${deployRecepientHashRaw}`,
+								blocked: false,
+							},
+						);*/
 
-  public publicKey(): CLPublicKey {
-    return this.keyPair.publicKey;
-  }
+						//console.log('Transfered token: ', deployTokenId);
+						//console.log('Transfered token recipient: ', deployRecepientHashRaw);
+					}
+				});
+			}
+		}
+	}
 
-  async listNfts(ids: string[]): Promise<Result<any[], ERROR_CODES>> {
-    const allAsyncResults = [];
+	public publicKey(): CLPublicKey {
+		return this.keyPair.publicKey;
+	}
 
-    for (const id of ids) {
-      try {
-        const meta = await this.contractClient.getTokenMeta(`${id}`);
-        const owner = await this.contractClient.getOwnerOf(`${id}`);
-        allAsyncResults.push({ metadata: Object.fromEntries(meta), id, owner });
-      } catch (e) {
-        console.error(`Error while querying ${id}`, e);
-      }
-    }
+	async listNfts(ids: string[]): Promise<Result<any[], ERROR_CODES>> {
+		const allAsyncResults = [];
 
-    if (allAsyncResults.length !== ids.length) {
-      return Err(ERROR_CODES.NftNotFoundOnChain);
-    }
+		for (const id of ids) {
+			try {
+				const meta = await this.contractClient.getMetadataOf(`${id}`);
+				const owner = await this.contractClient.getOwnerOf(`${id}`);
+				allAsyncResults.push({ metadata: meta, id, owner });
+			} catch (e) {
+				console.error(`Error while querying ${id}`, e);
+			}
+		}
 
-    return Ok(allAsyncResults);
-  }
+		if (allAsyncResults.length !== ids.length) {
+			return Err(ERROR_CODES.NftNotFoundOnChain);
+		}
 
-  async transferNFT(
-    nft: NFT,
-    recipient: string,
-  ): Promise<Result<string, ERROR_CODES>> {
-    const buffer = Buffer.from(recipient.split('-')[2], 'hex');
-    const recipientAH = new CLAccountHash(buffer);
+		return Ok(allAsyncResults);
+	}
 
-    const deploy = await this.contractClient.transfer(
-      recipientAH,
-      [nft.id],
-      '2000000000',
-      this.keyPair.publicKey,
-      [this.keyPair],
-    );
+	async transferNFT(
+		nft: NFT,
+		recipient: string,
+	): Promise<Result<string, ERROR_CODES>> {
+		const buffer = Buffer.from(recipient.split('-')[2], 'hex');
+		const recipientAH = new CLAccountHash(buffer);
 
-    try {
-      const hash = await deploy.send(CHAIN_RPC_URL);
-      this.pendingHashes.push(hash);
-      return Ok(hash);
-    } catch (err) {
-      console.error(err);
-      return Err(ERROR_CODES.InvalidDeploy);
-    }
-  }
+		const deploy = await this.contractClient.transfer(
+			{
+				source: this.keyPair.publicKey,
+				target: recipientAH,
+				tokenId: nft.id,
+			},
+			{ useSessionCode: false },
+			'2000000000',
+			this.keyPair.publicKey,
+			[this.keyPair],
+		);
+
+		try {
+			const hash = await deploy.send(CHAIN_RPC_URL);
+			this.pendingHashes.push(hash);
+			return Ok(hash);
+		} catch (err) {
+			console.error(err);
+			return Err(ERROR_CODES.InvalidDeploy);
+		}
+	}
 }
